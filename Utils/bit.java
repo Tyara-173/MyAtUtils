35b4bc1e045e1173181c729ed58d69ca2da59635
Tyara-173 <126230950+Tyara-173@users.noreply.github.com>
2023-12-04 16:31:06 +0900

    Add bit

---
+++ Utils/bit.java
@@ -0,0 +1,13 @@
+public class bit {
+    void bitAll(int n){
+        for (int i = 0; i < 1 << n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(((i >> j) & 1) == 1){
+
+                }else{
+
+                }
+            }
+        }
+    }
+}
