052fbcce7491f0be2df986e70d9e80bb273943c5
Tyara-173 <tyara1031@gmail.com>
2024-02-03 22:37:47 +0900

    [Add] LazySegTree

--- main.py
+++ main.py
@@ -419,6 +419,80 @@ utils = {
                 }
             }
         ''',
+    'lazyseg':
+        '''
+            class LazySegmentTree{
+                long inf = Long.MAX_VALUE;
+                private final int size;
+                private final long[] dat;
+                private final long[] lazy;
+                LazySegmentTree(int size){
+                    this.dat = new long[size*4];
+                    this.lazy = new long[size*4];
+                    int x = 1;
+                    while (x < size){
+                        x *= 2;
+                    }
+                    this.size = x;
+                }
+            
+                void eval(int k) { // 配列のk番目を更新
+                    if (lazy[k] == 0) return;  // 更新するものが無ければ終了
+                    if (k < size - 1) {             // 葉でなければ子に伝搬
+                        lazy[k * 2 + 1] = lazy[k];
+                        lazy[k * 2 + 2] = lazy[k];
+                    }
+                    // 自身を更新
+                    dat[k] = lazy[k];
+                    lazy[k] = 0;
+                }
+            
+                void update(int a, int b, long x, int k, int l, int r) {
+                    eval(k);
+                    if (a <= l && r <= b) {  // 完全に内側の時
+                        lazy[k] = x;
+                        eval(k);
+                    } else if (a < r && l < b) {                     // 一部区間が被る時
+                        update(a, b, x, k * 2 + 1, l, (l + r) / 2);  // 左の子
+                        update(a, b, x, k * 2 + 2, (l + r) / 2, r);  // 右の子
+                        dat[k] = Math.max(dat[k * 2 + 1], dat[k * 2 + 2]);
+                    }
+                }
+                void update(int a, int b, long x) { update(a, b, x, 0, 0, size); }
+            
+                long query(int a,int b){
+                    return query(a,b,0,0,size);
+                }
+            
+                long query(int a,int b,int k,int l,int r){
+                    eval(k);
+                    if(r <= a || b <= l){
+                        return 0;
+                    }
+                    if(a <= l && r <= b){
+                        return dat[k];
+                    }
+                    long vl = query(a,b,k*2+1,l,(l+r)/2);
+                    long vr = query(a,b,k*2+2,(l+r)/2,r);
+                    return Math.max(vl, vr);
+                }
+                List<List<Long>> getDat(){
+                    int x = 1;
+                    int index = 0;
+                    List<List<Long>> lists = new ArrayList<>();
+                    while (x <= size){
+                        List<Long> list = new ArrayList<>();
+                        for (int i = 0; i < x; i++) {
+                            list.add(dat[index]);
+                            index++;
+                        }
+                        lists.add(list);
+                        x *= 2;
+                    }
+                    return lists;
+                }
+            }
+        ''',
     'unionfind':
         '''
             class UnionFind{
@@ -651,6 +725,7 @@ with ui.card():
         ui.button('NodePair', on_click=lambda: copy('NodePair'))
     with ui.row():
         ui.button('セグ木', on_click=lambda: copy('segTree'))
+        ui.button('遅延セグ木', on_click=lambda: copy('lazyseg'))
         ui.button('UnionFind', on_click=lambda: copy('unionfind'))
 
 with ui.card():
