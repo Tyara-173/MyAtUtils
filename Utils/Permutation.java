79f79a6d5a0f9a92ab413156d7886def7329fe90
Tyara-173 <126230950+Tyara-173@users.noreply.github.com>
2024-10-15 10:15:53 +0900

    Update Permutation.java

--- Utils/Permutation.java
+++ Utils/Permutation.java
@@ -1,5 +1,27 @@
-class Permutation{
+class Permutation implements Iterator<int[]>, Iterable<int[]>{
     private List<List<Integer>> per;
+    private int[] arr;
+
+    Permutation(int n){
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return arr != null;
+    }
+
+    @Override
+    public int[] next() {
+        int[] temp = arr.clone();
+        if(!nextPermutation(arr)){
+            arr = null;
+        }
+        return temp;
+    }
     public List<List<Integer>> getPermutation(int size){
         if(per == null || per.size() != size){
             per = new ArrayList<>();
@@ -24,30 +46,22 @@ class Permutation{
         }
     }
 
-    List<Integer> nextPermutation(List<Integer> per){
-        int n = per.size();
-        List<Integer> list = new ArrayList<>(per);
-        TreeSet<Integer> set = new TreeSet<>();
-        int index = 0;
-        for (int i = 0; i < n-1; i++) {
-            if(list.get(i) < list.get(i+1)){
-                index = i;
-            }
+    boolean nextPermutation(int[] arr){
+        int len = arr.length;
+        int left = len - 2;
+        while (left >= 0 && arr[left] >= arr[left+1]) left--;
+        if (left < 0) return false;
+        int right = len - 1;
+        while (arr[left] >= arr[right]) right--;
+        { int t = arr[left]; arr[left] = arr[right];  arr[right] = t; }
+        left++;
+        right = len - 1;
+        while (left < right) {
+            { int t = arr[left]; arr[left] = arr[right]; arr[right] = t; }
+            left++;
+            right--;
         }
-        for (int i = index; i < n; i++) {
-            set.add(list.get(i));
-        }
-        int temp = list.get(index)+1;
-        while (!set.contains(temp)){
-            temp++;
-        }
-        set.remove(temp);
-        list.set(index,temp);
-        for (int i = index+1; i < n; i++) {
-            list.set(i,set.first());
-            set.remove(set.first());
-        }
-        return list;
+        return true;
     }
 
     List<Integer> prevPermutation(List<Integer> per){
@@ -74,5 +88,10 @@ class Permutation{
             set.remove(set.last());
         }
         return list;
+    }
+
+    @Override
+    public Iterator<int[]> iterator() {
+        return this;
     }
 }
